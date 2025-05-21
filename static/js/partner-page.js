05411f219916a5c17772373e18e68937ba6d5e3f
dozhdevlk <dima-03.adv@yandex.ru>
2025-05-21 23:14:17 +0300

    addLogsPhone

--- static/js/partner-page.js
+++ static/js/partner-page.js
@@ -104,6 +104,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			formData.append('servicePhone', document.getElementById('servicePhone').value);
 			formData.append('ownerName', document.getElementById('ownerName').value);
 			formData.append('ownerPhone', document.getElementById('ownerPhone').value);
+			console.log(document.getElementById('servicePhone').value)
 			console.log(document.getElementById('ownerPhone').value)
 			formData.append('ownerEmail', document.getElementById('ownerEmail').value);
 			formData.append('ownerPassword', document.getElementById('ownerPassword').value);
