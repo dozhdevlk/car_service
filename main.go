4ad95ac000cf6c7dc1880ef1ca1c6f54d9f7d420
dozhdevlk <dima-03.adv@yandex.ru>
2025-05-22 01:48:09 +0300

    fixed YMapps

--- main.go
+++ main.go
@@ -1857,35 +1857,35 @@ func pagePartnerHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func dashboardHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		http.Error(w, "Неверный ID партнера", http.StatusBadRequest)
-		return
-	}
-	session, err := store.Get(r, "session")
-	if err != nil {
-		http.Error(w, "Ошибка получения сессии", http.StatusInternalServerError)
-		return
-	}
-	userID, ok := session.Values["user_id"].(int)
-	if !ok {
-		http.Error(w, "Пользователь не авторизован", http.StatusUnauthorized)
-		return
-	}
-	var partnerOwnerId int
-	err = db.QueryRow(`
-		SELECT s.owner_id
-		FROM services s
-		WHERE s.id = $1 AND s.owner_id = $2
-	`, id, userID).Scan(&partnerOwnerId)
-	if err == sql.ErrNoRows {
-		http.Error(w, "У вас нет доступа к этой панели", http.StatusForbidden)
-		return
-	} else if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	// vars := mux.Vars(r)
+	// id, err := strconv.Atoi(vars["id"])
+	// if err != nil {
+	// 	http.Error(w, "Неверный ID партнера", http.StatusBadRequest)
+	// 	return
+	// }
+	// session, err := store.Get(r, "session")
+	// if err != nil {
+	// 	http.Error(w, "Ошибка получения сессии", http.StatusInternalServerError)
+	// 	return
+	// }
+	// userID, ok := session.Values["user_id"].(int)
+	// if !ok {
+	// 	http.Error(w, "Пользователь не авторизован", http.StatusUnauthorized)
+	// 	return
+	// }
+	// var partnerOwnerId int
+	// err = db.QueryRow(`
+	// 	SELECT s.owner_id
+	// 	FROM services s
+	// 	WHERE s.id = $1 AND s.owner_id = $2
+	// `, id, userID).Scan(&partnerOwnerId)
+	// if (err == sql.ErrNoRows && ) {
+	// 	http.Error(w, "У вас нет доступа к этой панели", http.StatusForbidden)
+	// 	return
+	// } else if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
 	tmpl, err := template.ParseFiles("templates/dashboard.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
